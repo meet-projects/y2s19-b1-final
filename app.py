96698266dffb96170d504428c06fa3384f5e47c8
myname21-meet <myname21@meet.mit.edu>
2019-08-06 07:12:57 -0400

    Fixed session name

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 9# Flask-related imports
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import Flask, render_template, url_for, redirect, request
+from flask import session as login_session
 
 # Add functions you need from databases.py to the next line!
 from databases import *
@@ -15,14 +16,14 @@ def home():
 @app.route('/new_flavor', methods= ['GET', 'POST'])
 def create_flavor():
 	if request.method == "POST":
-		session['flavor_id'] = add_Flavor(request.form['name'],request.form['flavor'],request.form['add_on'])
+		login_session['flavor_id'] = add_Flavor(request.form['name'],request.form['flavor'],request.form['add_on'])
 		return render_template('submit_flavor.html')
 	return render_template("create_flavor1.html")
 
 @app.route('/submit_flavor', methods= ['GET', 'POST'])
 def submit_flavor():
 	if request.method == "POST":
-		add_user(request.form['name'],request.form['email'], session['flavor_id'])
+		add_user(request.form['name'],request.form['email'], login_session['flavor_id'])
 		return render_template('thanku.html')
 	return render_template("submit_flavor.html")
 
